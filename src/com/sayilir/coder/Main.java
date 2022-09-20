06bd592b0793d632df1b015969c38bd59e2b28da
eyp <eypsay@yahoo.com>
2022-09-20 14:55:43 +0300

    HW-week1

--- src/com/sayilir/coder/Main.java
+++ src/com/sayilir/coder/Main.java
@@ -6,12 +6,19 @@ import com.sayilir.coder.classTraining.ProductManager;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        ReCapDemo2 reCapDemo2=new ReCapDemo2();
+        // write your code here
+        ReCapDemo2 reCapDemo2 = new ReCapDemo2();
         reCapDemo2.runReCapDemo2();
 
-        WonderNumber wonderNumber= new WonderNumber();
+        WonderNumber wonderNumber = new WonderNumber();
         wonderNumber.wonderNumberrun();
+
+        MultiDimensionalArray multiDimensionalArray = new MultiDimensionalArray();
+        multiDimensionalArray.multiDimensionalArrayrun();
+        ArraysTrain arraysTrain = new ArraysTrain();
+        arraysTrain.runArraysTrain();
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        primeNumbers.primeNumbersrun();
 
 
         Product product = new Product();
